da0c1bb535c5394c1dd1e691f5530ade9599f2e0
Issei Naruta <naruta@ivry.jp>
2024-09-10 15:43:49 +0900

    Refactor patches and implement postgres specific patches

--- lib/arproxy/connection_adapter_patch.rb
+++ lib/arproxy/connection_adapter_patch.rb
@@ -4,24 +4,32 @@ module Arproxy
 
     def initialize(adapter_class)
       @adapter_class = adapter_class
-      @enabled_patches = Set.new
+      @applied_patches = Set.new
     end
 
     def enable!
       case adapter_class::ADAPTER_NAME
-      when 'Mysql2', 'Trilogy' # known children of AbstractMysqlAdapter
+      when 'Mysql2', 'Trilogy'
         if ActiveRecord.version >= '7.0'
-          enable_patch :raw_execute
+          apply_patch :raw_execute
         else
-          enable_patch :execute
+          apply_patch :execute
         end
-      when 'PostgreSQL', 'SQLServer', 'SQLite' # known children of AbstractAdapter
+      when 'PostgreSQL'
         if ActiveRecord.version >= '7.1'
-          enable_patch :raw_execute
-          enable_patch :internal_exec_query
+          apply_patch :raw_execute
         else
-          enable_patch :execute
-          enable_patch :exec_query
+          apply_patch :execute
+        end
+        apply_patch_binds :exec_no_cache
+        apply_patch_binds :exec_cache
+      when 'SQLServer', 'SQLite'
+        if ActiveRecord.version >= '7.1'
+          apply_patch :raw_execute
+          apply_patch_binds :internal_exec_query
+        else
+          apply_patch :execute
+          apply_patch_binds :exec_query
         end
       else
         raise Arproxy::Error, "Unexpected connection adapter: #{adapter_class&.name}"
@@ -30,57 +38,45 @@ module Arproxy
     end
 
     def disable!
-      @enabled_patches.dup.each do |target_method|
+      @applied_patches.dup.each do |target_method|
         adapter_class.class_eval do
           if respond_to?(:"#{target_method}_with_arproxy")
             alias_method target_method, :"#{target_method}_without_arproxy"
             remove_method :"#{target_method}_with_arproxy"
           end
         end
-        @enabled_patches.delete(target_method)
+        @applied_patches.delete(target_method)
       end
       ::Arproxy.logger.debug("Arproxy: Disabled (#{adapter_class::ADAPTER_NAME})")
     end
 
     private
-      def enable_patch(target_method)
-        return if @enabled_patches.include?(target_method)
-
+      def apply_patch(target_method)
+        return if @applied_patches.include?(target_method)
         adapter_class.class_eval do
-          case target_method
-          when :execute # for AbstractMysqlAdapter, ActiveRecord 6.1 and earlier
-            def execute_with_arproxy(sql, name=nil, **kwargs) #
-              ::Arproxy.proxy_chain.connection = self
-              _sql, _name = *::Arproxy.proxy_chain.head.execute(sql, name)
-              self.send(:execute_without_arproxy, _sql, _name, **kwargs)
-            end
-          when :exec_query # for AbstractAdapter, ActiveRecord 7.0 and earlier
-            def exec_query_with_arproxy(sql, name=nil, binds=[], **kwargs) #
-              ::Arproxy.proxy_chain.connection = self
-              _sql, _name = *::Arproxy.proxy_chain.head.execute(sql, name)
-              self.send(:exec_query_without_arproxy, _sql, _name, binds, **kwargs)
-            end
-          when :raw_execute
-            def raw_execute_with_arproxy(sql, name=nil, **kwargs)
-              ::Arproxy.proxy_chain.connection = self
-              _sql, _name = *::Arproxy.proxy_chain.head.execute(sql, name)
-              self.send(:raw_execute_without_arproxy, _sql, _name, **kwargs)
-            end
-          when :internal_exec_query
-            def internal_exec_query_with_arproxy(sql, name=nil, binds=[], **kwargs)
-              ::Arproxy.proxy_chain.connection = self
-              _sql, _name = *::Arproxy.proxy_chain.head.execute(sql, name)
-              self.send(:internal_exec_query_without_arproxy, _sql, _name, binds, **kwargs)
-            end
-          else
-            raise ArgumentError, "Unsupported method to patch: #{target_method}"
+          define_method("#{target_method}_with_arproxy") do |sql, name=nil, **kwargs|
+            ::Arproxy.proxy_chain.connection = self
+            _sql, _name = *::Arproxy.proxy_chain.head.execute(sql, name)
+            self.send("#{target_method}_without_arproxy", _sql, _name, **kwargs)
           end
-
           alias_method :"#{target_method}_without_arproxy", target_method
           alias_method target_method, :"#{target_method}_with_arproxy"
         end
+        @applied_patches << target_method
+      end
 
-        @enabled_patches << target_method
+      def apply_patch_binds(target_method)
+        return if @applied_patches.include?(target_method)
+        adapter_class.class_eval do
+          define_method("#{target_method}_with_arproxy") do |sql, name=nil, binds=[], **kwargs|
+            ::Arproxy.proxy_chain.connection = self
+            _sql, _name = *::Arproxy.proxy_chain.head.execute(sql, name)
+            self.send("#{target_method}_without_arproxy", _sql, _name, binds, **kwargs)
+          end
+          alias_method :"#{target_method}_without_arproxy", target_method
+          alias_method target_method, :"#{target_method}_with_arproxy"
+        end
+        @applied_patches << target_method
       end
   end
 end
